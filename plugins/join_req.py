53006796ac72042aab944c08b941b5a4dba92842
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-11-30 07:32:38 +0530

    Add SYD_CHANNEL import to join_req.py

--- plugins/join_req.py
+++ plugins/join_req.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import ChatJoinRequest, InlineKeyboardButton, InlineKeyboardMarkup
 from database.users_chats_db import db
-from info import ADMINS, AUTH_CHANNEL
+from info import ADMINS, AUTH_CHANNEL, SYD_CHANNEL
 
 @Client.on_chat_join_request(filters.chat(AUTH_CHANNEL))
 async def join_reqs(client, message: ChatJoinRequest):
