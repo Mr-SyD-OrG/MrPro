3937c056eedf4833a7ac13be75ce3a2b15bbfba1
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-12-06 09:48:18 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -251,7 +251,7 @@ Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
 """ 
    # PLEASE DO NOT REMOVE ANY CREDITS ❤️‍🩹
     
-    CAPTION = """ 📂 <i><a href="https://t.me/+Uqdj6CEYypVlNDA9">{file_name}</a>\n{sydaudcap}</i>\n"""
+    CAPTION = """ 📂 <b><i><a href="https://t.me/+Uqdj6CEYypVlNDA9">{file_name}</a>\n{sydaudcap}</i></b>\n"""
 
     IMDB_TEMPLATE_TXT = """
 <b>Hᴇʏ {message.from_user.mention}, ʜᴇʀᴇ ɪꜱ ᴛʜᴇ ʀᴇꜱᴜʟᴛꜱ ꜰᴏʀ ʏᴏᴜʀ ǫᴜᴇʀʏ {search}.
